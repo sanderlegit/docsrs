fcd3346d876a7bfd6c0fcf94277f036ed5c35bae
Jonas Arter <jonasarter@outlook.com>
2025-07-07 14:13:17 +0200

    added get and get_path to easily get some items and itemsummaries

--- src/doc/indexed.rs
+++ src/doc/indexed.rs
@@ -2,7 +2,7 @@ mod search;
 mod search_index;
 
 use super::Doc;
-use rustdoc_types::{Crate, Id};
+use rustdoc_types::{Crate, Id, Item, ItemSummary};
 
 pub struct Indexed {
     pub ast: Crate,
@@ -17,5 +17,13 @@ impl Doc<Indexed> {
             search_index: None,
             matcher: fuzzy_matcher::skim::SkimMatcherV2::default(),
         })
+    }
+
+    pub fn get(&self, id: u32) -> Option<&Item> {
+        self.0.ast.index.get(&Id(id))
+    }
+
+    pub fn get_path(&self, id: u32) -> Option<&ItemSummary> {
+        self.0.ast.paths.get(&Id(id))
     }
 }
