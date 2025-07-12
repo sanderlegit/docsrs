c9e8c68748f533cecde97edeedb55d9044edff94
Jonas Arter <jonasarter@outlook.com>
2025-07-12 11:48:33 +0200

    changed function order

--- src/doc/parsed/index.rs
+++ src/doc/parsed/index.rs
@@ -1,9 +1,23 @@
 use super::{Doc, Parsed};
 use crate::{Indexed, doc::indexed::SearchKey};
-use rustdoc_types::{Crate, Id, Impl, Item};
+use rustdoc_types::{Crate, Id, Impl, Item, ItemEnum};
 use std::collections::HashMap;
 
 impl Doc<Parsed> {
+    pub fn build_search_index(&self) -> Doc<Indexed> {
+        let krate = &self.0.ast;
+        let index = krate
+            .index
+            .iter()
+            .filter_map(|(id, item)| self.generate_searchkeys(id, item))
+            .flat_map(|vec| vec.into_iter())
+            .collect();
+
+        let items = HashMap::new();
+
+        Doc::<Indexed>::new(index, items)
+    }
+
     fn generate_searchkeys(&self, id: &Id, item: &Item) -> Option<Vec<SearchKey>> {
         let krate = &self.0.ast;
         let mut search_keys = Vec::new();
@@ -14,17 +28,13 @@ impl Doc<Parsed> {
             key: base_path.clone(),
         });
 
-        use rustdoc_types::ItemEnum;
-
         match &item.inner {
             ItemEnum::Struct(strukt) => {
                 search_keys.extend(Self::search_keys_structs(krate, strukt, &base_path));
             }
-
             ItemEnum::Enum(_) => {
                 search_keys.extend(Self::search_keys_enums(krate, id, &base_path));
             }
-
             _ => {}
         }
 
@@ -44,19 +54,5 @@ impl Doc<Parsed> {
                 key: format!("{base_path}::{name}"),
             })
         })
-    }
-
-    pub fn build_search_index(&self) -> Doc<Indexed> {
-        let krate = &self.0.ast;
-        let index = krate
-            .index
-            .iter()
-            .filter_map(|(id, item)| self.generate_searchkeys(id, item))
-            .flat_map(|vec| vec.into_iter())
-            .collect();
-
-        let items = HashMap::new();
-
-        Doc::<Indexed>::new(index, items)
     }
 }
